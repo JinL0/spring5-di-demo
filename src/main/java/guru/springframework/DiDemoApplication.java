6ba35ad9c019a433d19e4b91445da17a7f8a3b5f
jin liu <stuc.cs.jin@berkeley.edu>
2019-08-11 22:00:58 -0700

    forgot the annotation

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -11,6 +11,8 @@ public class DiDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
-		MyController controller = (MyController) ctx.getBean("MyController");
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		controller.hello();
 	}
 }
--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -1,5 +1,8 @@
 package guru.springframework.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MyController {
     public void hello(){
         System.out.println("hello");
